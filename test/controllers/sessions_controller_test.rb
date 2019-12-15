5ca8060fe6a233d6c2cc2c410a5b4fc4c414e64a
KoseiYoshida <koseiasengineer@gmail.com>
2019-12-15 10:51:54 +0000

    Finish user edit, update, index and destory actions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,9 +2,18 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:michael)
+  end
+  
   test "should get new" do
     get login_path
     assert_response :success
   end
-
+  
+  test "should not redirect forwarding_url again" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_nil session[:forwarding_url]
+  end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -15,4 +15,22 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Sign up")
   end
   
+  test "layout links with logged in" do
+    @user = users(:michael)
+    log_in_as(@user)
+    get root_path
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
+    get users_path
+    assert_select "title", full_title("All users")
+  end
+  
 end
