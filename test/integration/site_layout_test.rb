10e8d36592969136abfa73478f799081cc386d69
KoseiYoshida <koseiasengineer@gmail.com>
2020-01-02 09:11:42 +0000

    Add user following

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -25,6 +25,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", following_user_path(@user)
+    assert_select "a[href=?]", followers_user_path(@user)
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
